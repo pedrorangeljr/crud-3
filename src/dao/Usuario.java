23961d2fdd09efa5b003ec5684edc89c54663e25
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2023-03-23 11:15:12 -0300

    Classe Usuario

---
+++ src/dao/Usuario.java
@@ -0,0 +1,36 @@
+package dao;
+
+public class Usuario {
+
+	private Long id;
+	private String nome;
+	private String sobrenome;
+	private String email;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
